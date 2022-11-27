38a546d50ad41450d34489f111d15df35d15c0f7
Leontij <mrleonchik@gmail.com>
2022-11-27 18:00:54 +0300

    Первое задание выполнено

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+Console.Write("Введите трехзначное число:");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = (a % 100 - a % 10)/10;
+Console.WriteLine(result);
